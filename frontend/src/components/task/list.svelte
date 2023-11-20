c4ec23ce7b6dcbc3c54e8d6c1962a07337b51bf3
Ofilio Herdocia Aráuz <Herdocia20@gmail.com>
2023-11-20 16:18:33 -0600

    Testing Svelte

---
+++ frontend/src/components/task/list.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<table>
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th colspan="3">Rondas</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import TaskList from "../components/task/list.svelte";
+  import { Button } from "@svelteuidev/core";
+  import { Container } from "@svelteuidev/core";
+  import { Grid } from "@svelteuidev/core";
+
+  let clicked = false;
+</script>
+
+<Container>
+  <Grid>
+    <Grid.Col md={8}>
+      <h1>Welcome to SvelteKit</h1>
+      <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+      </p>
+      <TaskList />
+      <Button on:click={() => (clicked = !clicked)} class="animation-bounce">
+        {clicked ? "Don't click me anymore" : "Click me"}
+      </Button>
+    </Grid.Col>
+  </Grid>
+</Container>

10faaab9f0d4158d21e880c7e963a0ef001ea6ca
Ofilio Herdocia Aráuz <Herdocia20@gmail.com>
2023-11-21 15:50:50 -0600

    Testing relations

--- backend/app/Entities/Models/personal.py
+++ backend/app/Entities/Models/personal.py
@@ -1,17 +1,19 @@
 from sqlalchemy_serializer import SerializerMixin
 from extensions.alchemy import alchemy
+from .catalogs import Gender
 
 
 class Personal(alchemy.Model, SerializerMixin):
 
     id = alchemy.Column(alchemy.Integer, primary_key=True)
-    gender_id = alchemy.Column(alchemy.Integer, alchemy.ForeignKey("genders.id"), nullable=False)
+    gender_id = alchemy.Column(alchemy.Integer, alchemy.ForeignKey(Gender.id), nullable=False)
     firstnames = alchemy.Column(alchemy.String, nullable=False)
     lastnames = alchemy.Column(alchemy.String, nullable=False)
     birthday = alchemy.Column(alchemy.String)
     active = alchemy.Column(alchemy.Boolean, default=True)
 
-    gender = alchemy.relationship("Gender", backref=alchemy.backref("personal", lazy="dynamic"))
+    #gender = alchemy.relationship("Gender", backref=alchemy.backref("personal", lazy="dynamic"))
+    gender = alchemy.relationship(Gender, lazy='select')
     serialize_rules = ()
 
     def __init__(self, firstname, lastname, birthday, gender_id, type_id):
--- backend/extensions/guard.py
+++ backend/extensions/guard.py
@@ -2,10 +2,12 @@ from flask_praetorian import Praetorian
 from app.Entities import User
 import os
 
-guard = Praetorian();
+guard = Praetorian()
+
 
 def register_guard(app):
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-    app.config["JWT_ACCESS_LIFESPAN"] = {"hours": int(os.getenv("JWT_ACC_LFSPN"))}
-    app.config["JWT_REFRESH_LIFESPAN"] = {"days": int(os.getenv("JWT_REF_LFSPN"))}
-    guard.init_app(app, User);
+    with app.app_context():
+        app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+        app.config["JWT_ACCESS_LIFESPAN"] = {"hours": int(os.getenv("JWT_ACC_LFSPN"))}
+        app.config["JWT_REFRESH_LIFESPAN"] = {"days": int(os.getenv("JWT_REF_LFSPN"))}
+        guard.init_app(app, User)

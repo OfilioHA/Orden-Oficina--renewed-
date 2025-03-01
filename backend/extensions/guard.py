707a6a7909cb2f2b5012f7f418dbe866ab585cc4
OfilioHA <herdocia20@gmail.com>
2025-03-01 05:42:01 -0600

    feat: :sparkles: Create a testing user if not exists

--- backend/extensions/guard.py
+++ backend/extensions/guard.py
@@ -1,5 +1,6 @@
 from flask_praetorian import Praetorian
 from app.Entities import User
+from extensions.alchemy import alchemy as db
 import os
 
 guard = Praetorian()
@@ -11,3 +12,18 @@ def register_guard(app):
         app.config["JWT_ACCESS_LIFESPAN"] = {"hours": int(os.getenv("JWT_ACC_LFSPN"))}
         app.config["JWT_REFRESH_LIFESPAN"] = {"days": int(os.getenv("JWT_REF_LFSPN"))}
         guard.init_app(app, User)
+        create_user()
+
+def create_user():
+    username = "testing"
+    password = "password"
+
+    user = db.session.query(User).filter_by(username=username).first()
+
+    if not user:
+        new_user = User()
+        new_user.username = username
+        new_user.password = guard.hash_password(password)
+        new_user.personal_id = 1
+        db.session.add(new_user)
+        db.session.commit()

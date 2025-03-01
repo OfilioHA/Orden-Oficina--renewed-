01dbea000245c788e1b8c25010891c172921bf68
OfilioHA <herdocia20@gmail.com>
2025-03-01 05:44:00 -0600

    feat: :sparkles: New axios interceptor for adding the token if exists

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookie from 'js-cookie';
 
 export function createAxios(url, config) {
   return axios.create({
@@ -10,5 +11,15 @@ export function createAxios(url, config) {
 export function createServerAxios() {
   const url = import.meta.env.VITE_SERVER_URL;
   const axios = createAxios(url);
+
+  axios.interceptors.request.use((config) => {
+      const token = Cookie.get('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    }, (error) => Promise.reject(error)
+  );
+
   return axios;
 }
